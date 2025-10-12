f2af10b8a25db2d593333b0f60a4e5c7a072a39a
Xenia Golbraykh <karzerin@gmail.com>
2025-10-12 15:43:33 +0300

    fix generateNumber

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,13 @@
+import { webcrypto } from 'node:crypto'
+
+const { getRandomValues } = webcrypto
+
 export const generateNumber = (min = 0, max = 100) => {
   const range = max - min + 1
   if (range <= 0 || range > 2 ** 32) {
     throw new Error('Invalid range')
   }
 
-  const randomUint32 = crypto.getRandomValues(new Uint32Array(1))[0]
+  const randomUint32 = getRandomValues(new Uint32Array(1))[0]
   return min + (randomUint32 % range)
 }
