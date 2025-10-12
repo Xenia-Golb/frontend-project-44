d9b51425617272c2dc868848ff58fed7453f092f
Xenia Golbraykh <karzerin@gmail.com>
2025-10-12 15:30:54 +0300

    fix generateNumber

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,9 @@
 export const generateNumber = (min = 0, max = 100) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  const range = max - min + 1
+  if (range <= 0 || range > 2 ** 32) {
+    throw new Error('Invalid range')
+  }
+
+  const randomUint32 = crypto.getRandomValues(new Uint32Array(1))[0]
+  return min + (randomUint32 % range)
 }
