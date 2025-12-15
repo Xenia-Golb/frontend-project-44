c7abb68056e54afdad9342fd52c1e40d8054787c
Xenia Golbraykh <karzerin@gmail.com>
2025-12-15 20:13:26 +0300

    fix funct progress

--- src/games/progression.js
+++ src/games/progression.js
@@ -10,16 +10,13 @@ const generateProgression = (start, step, length) => {
 
 const getQuestionAndAnswer = () => {
   const start = generateNumber(1, 100)
-  const step = generateNumber(1, 100)
+  const step = generateNumber(1, 10)
   const length = 10
-  const hiddenIndex = generateNumber(1, 100)
+  const hiddenIndex = generateNumber(0, length - 1)
 
   const progression = generateProgression(start, step, length)
-
   const correctAnswer = String(progression[hiddenIndex])
-
   progression[hiddenIndex] = '..'
-
   const question = progression.join(' ')
 
   return [question, correctAnswer]
