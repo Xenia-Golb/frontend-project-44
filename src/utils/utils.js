7b569e434973e56f797271b96fb08439ee1fdca2
Xenia Golbraykh <karzerin@gmail.com>
2025-12-15 19:36:09 +0300

    style: fix lint errors

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,13 +1,11 @@
 import { webcrypto } from 'node:crypto'
 
-const { getRandomValues } = webcrypto
-
 export const generateNumber = (min = 0, max = 100) => {
   const range = max - min + 1
   if (range <= 0 || range > 2 ** 32) {
     throw new Error('Invalid range')
   }
-
-  const randomUint32 = getRandomValues(new Uint32Array(1))[0]
-  return min + (randomUint32 % range)
+  const array = new Uint32Array(1)
+  webcrypto.getRandomValues(array)
+  return min + (array[0] % range)
 }
